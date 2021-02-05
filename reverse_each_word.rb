3c5e3fd8e8827c08d7578d2fb7259ffefe0977bb
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-05 05:27:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
-def reverse_each_word
-  sort_by.with_index {|_, i| -i}
- end
+def reverse_each_word(string)
+  new_arr - [1]
+  arr = string.split("")
+  arr.each {|i| new_arr << i.reverse}
+  new_arr.join("")
 end
